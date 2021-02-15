2f9d1bcddfc373e6c0c524d4b9e4da1bb53afb84
Rebecca Leung <rjleung@uci.edu>
2021-02-15 23:46:21 +0000

    Adding response status

--- routes/likes.js
+++ routes/likes.js
@@ -12,7 +12,7 @@ router.post('/like', function(req, res) {
     }
     const newLike = new Like(query);
     newLike.save()
-    .then(() => res.send("liked"))
+    .then(() => res.status(200).send("liked"))
     .catch(err => res.status(400).json("Error: " +  err))
 
     Post.updateOne({_id: new mongoose.Types.ObjectId(req.body.postId)}, {$inc: {likes: 1}}, function(err, docs) {
@@ -27,16 +27,16 @@ router.post('/like', function(req, res) {
 
 router.post('/unlike', function(req, res) {
     Like.deleteOne({_id: new mongoose.Types.ObjectId(req.body.id)}).then(function() {
-        res.send("like deleted");
+        res.status(200).send("like deleted");
     }).catch(function(error) {
-        res.send(error);
+        res.status(400).send(error);
     });
     Post.updateOne({_id: new mongoose.Types.ObjectId(req.body.postId)}, {$inc: { likes: -1}}, function(err, docs) {
         if (err) {
-            res.send(err);
+            res.status(400).send(err);
         }
         else {
-            res.send("unliked");
+            res.status(200).send("unliked");
         }
     });
 });
