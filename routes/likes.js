04dfad1bdca4d61e221227537d5ac26034b41072
henryg0 <hgip@uci.edu>
2021-05-11 20:51:48 +0000

    Updated error checking for unlike

--- routes/likes.js
+++ routes/likes.js
@@ -1,54 +1,202 @@
 const router = require('express').Router();
 const mongoose = require('mongoose');
-const { check, validationResult } = require('express-validator');
+const {
+  check,
+  validationResult
+} = require('express-validator');
 const Like = require('../models/like.model');
 const Post = require('../models/post.model');
 const User = require('../models/user.model');
+const {
+  whetherEmailIsRegistered
+} = require('../utils');
 
-router.post('/like', async function(req, res) {
-    query = {
-        "_id": new mongoose.Types.ObjectId(),
-        "username": req.body.username,
-        "date": new Date(),
-        "postId": new mongoose.Types.ObjectId(req.body.postId)
+
+router.route('/like').post([
+  check('username')
+    .notEmpty()
+    .withMessage("Invalid username, should not be empty")
+    .isLength({
+      min: 1,
+      max: 100
+    })
+    .withMessage("Invalid username length, should be between 1, 100."),
+  check('postId').notEmpty().withMessage("Invalid post id, should not be empty")
+], async function (req, res) {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({
+      message: errors.array()
+    })
+  }
+  query = {
+    "_id": new mongoose.Types.ObjectId(),
+    "username": req.body.username,
+    "date": new Date(),
+    "postId": req.body.postId
+  }
+
+  try {
+    const docs = await Like.findOne({
+      $and: [{
+          "username": req.body.username
+        },
+        {
+          "postId": req.body.postId
+        }
+      ]
+    })
+
+    if (docs) {
+      res.status(400).json({
+        errMsg: "error, like was already found",
+        likeDoc: docs,
+      })
+    } else if (!await whetherEmailIsRegistered(req.body.username)) {
+      res.status(400).json({
+        errMsg: "error, user is not registered",
+      })
     }
-    const newLike = new Like(query);
-    await newLike.save().then(() => res.status(200).send("liked")).catch(err => res.status(400).json("Error: " +  err))
+    // like doesn't already exist, create new one
+    else {
+      const newLike = new Like(query);
+      await newLike.save().then();
 
-    // increment likes field in post
-    await Post.updateOne({_id: new mongoose.Types.ObjectId(req.body.postId)}, {$inc: {numLikes: 1}});
+      // increment likes field in post
+      await Post.updateOne({
+        _id: req.body.postId
+      }, {
+        $inc: {
+          numLikes: 1
+        }
+      });
 
-    // append to likedUsers array in post
-    await Post.updateOne({_id: new mongoose.Types.ObjectId(req.body.postId)}, {$push: {likedUsers: req.body.username}});
+      // append to likedUsers array in post
+      await Post.updateOne({
+        _id: req.body.postId
+      }, {
+        $push: {
+          likedUsers: req.body.username
+        }
+      });
 
-    // append to likedPosts in user
-    await User.updateOne({username: req.body.username}, {$push: {likedPosts: req.body.postId}});
+      // append to likedPosts in user
+      await User.updateOne({
+        username: req.body.username
+      }, {
+        $push: {
+          likedPosts: req.body.postId
+        }
+      });
+      res.status(200).send("liked");
+    }
+  } catch (err) {
+    // error w/ fetching document
+    res.status(400).json({
+      error: err
+    })
+  }
 });
 
-router.post('/unlike', async function(req, res) {
-    // delete like
-    await Like.deleteOne({_id: new mongoose.Types.ObjectId(req.body.id)}).then(function() {
-        res.status(200).send("like deleted");
-    }).catch(function(error) {
-        res.status(400).send(error);
+
+
+router.route('/unlike').post([
+  check('username')
+    .notEmpty()
+    .withMessage("Invalid username, should not be empty")
+    .isLength({
+      min: 1,
+      max: 100
+    })
+    .withMessage("Invalid username length, should be between 1, 100."),
+  check('postId').notEmpty().withMessage("Invalid post id, should not be empty"),
+], async function (req, res) {
+  // check for errs
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({
+      message: errors.array()
+    })
+  }
+
+  try {
+    const docs = await Like.findOne({
+      $and: [{
+          "username": req.body.username
+        },
+        {
+          "postId": req.body.postId
+        }
+      ]
     });
+    if (!docs) {
+      res.status(400).json({
+        errMsg: "error, could not find like",
+        likeDoc: docs,
+      })
+    }
+    // sanity check to see if user is registered (will never hit this error though since the above error will be caught first )
+    else if (!await whetherEmailIsRegistered(req.body.username)) {
+      res.status(400).json({
+        errMsg: "error, user is not registered",
+      })
+    } else {
+      await Like.deleteOne({
+        $and: [{
+            "username": req.body.username
+          },
+          {
+            "postId": req.body.postId
+          }
+        ]
+      })
 
-    // decrement likes field in post
-    await Post.updateOne({_id: new mongoose.Types.ObjectId(req.body.postId)}, {$inc: { numLikes: -1}});
+      // decrement likes field in post
+      await Post.updateOne({
+        _id: req.body.postId
+      }, {
+        $inc: {
+          numLikes: -1
+        }
+      });
 
-    // remove from likedUsers array in post
-    await Post.updateOne({_id: new mongoose.Types.ObjectId(req.body.postId)}, {$pull: {likedUsers: req.body.username}});
+      // remove from likedUsers array in post
+      await Post.updateOne({
+        _id: req.body.postId
+      }, {
+        $pull: {
+          likedUsers: req.body.username
+        }
+      });
 
-    // remove from likedPosts in user
-    await User.updateOne({username: req.body.username}, {$pull: {likedPosts: req.body.postId}});
+      // remove from likedPosts in user
+      await User.updateOne({
+        username: req.body.username
+      }, {
+        $pull: {
+          likedPosts: req.body.postId
+        }
+      });
+      res.status(200).json({
+        success: docs
+      });
+    }
+  } catch (err) {
+    res.status(400).json({
+      errors: err
+    })
+  }
 });
 
 router.post(
   "/byUser",
   [
     check("username")
-      .isLength({ min: 1, max: 100 })
-      .withMessage("Invalid username length, should be between 1, 100."),
+    .isLength({
+      min: 1,
+      max: 100
+    })
+    .withMessage("Invalid username length, should be between 1, 100."),
   ],
   async (req, res) => {
     const errors = validationResult(req);
@@ -58,7 +206,9 @@ router.post(
       });
     }
 
-    const { username } = req.body;
+    const {
+      username
+    } = req.body;
 
     try {
       let likedPostIds = await Like.find()
@@ -66,10 +216,10 @@ router.post(
         .select("postId -_id")
         .exec();
       likedPostIds = likedPostIds.map((likedPostId) => likedPostId.postId);
-    //   console.log(likedPostIds);
+      //   console.log(likedPostIds);
 
       const likedPosts = await Post.find().where("_id", likedPostIds).exec();
-    //   console.log(likedPosts);
+      //   console.log(likedPosts);
 
       res.status(200).json(likedPosts);
     } catch (err) {
